8b0297df3e6112b881bf022ff498b9db6e374fbf
Jorik Schut <jorikschut@air-van-jorik.home>
2026-02-13 14:20:45 +0100

    Implement SEO improvements, Sitemap, Image optimizations, and Responsive fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://voorste-eng.nl',
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "nl",
     locales: ["nl", "en", "de"],
